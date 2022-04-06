3b7b84708156d3b136ce8423fc5345946d1c51c4
christian <christiangouw168@gmail.com>
2022-04-06 22:20:43 +0700

    refactor: make enum become global

---
+++ FoodJournal/Model/GlobalEnum.swift
@@ -0,0 +1,18 @@
+//
+//  GlobalEnum.swift
+//  FoodJournal
+//
+//  Created by christian gouw on 06/04/22.
+//
+
+import Foundation
+
+enum FormCategory: String, CaseIterable {
+    case meal = "meal"
+    case beverage = "beverage"
+}
+
+enum Category: String {
+    case meal = "meal"
+    case beverage = "beverage"
+}
--- FoodJournal/Model/TodayMealModel.swift
+++ FoodJournal/Model/TodayMealModel.swift
@@ -7,11 +7,6 @@
 
 import Foundation
 
-enum Category: String {
-    case meal = "meal"
-    case beverage = "beverage"
-}
-
 struct TodayMeals {
     let id: Int
     let name:String
@@ -26,5 +21,7 @@ extension TodayMeals {
     static let all: [TodayMeals] = [
         TodayMeals(id: 1, name: "Dimsum", time: "20:00", timesOfEat: 1, image: "Dimsum", category: "meal", reflections: ["The taste", "It was time"]),
         TodayMeals(id: 2, name: "Omelet", time: "21:00", timesOfEat: 2, image: "Omelet", category: "meal", reflections: ["Why not ?"]),
+        TodayMeals(id: 3, name: "Pancake", time: "22:00", timesOfEat: 3, image: "Pancake", category: "meal", reflections: ["Why not ?", "Social"]),
+        TodayMeals(id: 4, name: "Spagethi", time:"23:00", timesOfEat: 4, image: "Spagethi", category: "meal", reflections: ["Hungry", "Bored"])
     ]
 }
