9dee6b83a999b0d46cf6fb7b57021e63f3e8a480
christian <christiangouw168@gmail.com>
2022-04-12 11:01:39 +0700

    feat: daily recap

--- FoodJournal/Views/Details/DailyRecap.swift
+++ FoodJournal/Views/Details/DailyRecap.swift
@@ -8,13 +8,33 @@
 import SwiftUI
 
 struct DailyRecap: View {
+    
+    @Binding var allRecap: [DailyRecaps]
+    
     var body: some View {
-        Text("Daily Recap Here")
+        ScrollView(.vertical, showsIndicators: false){
+            LazyVStack {
+                ForEach(allRecap, id: \.id){ recap in
+//                    AllRecapCard(daily: recap)
+                    NavigationLink(destination: RecapHistory(history: recap)){
+                        AllRecapCard(daily: recap)
+                    }
+                }
+            }
+        }
     }
 }
 
 struct DailyRecap_Previews: PreviewProvider {
+    
+    @State private static var dummy: [DailyRecaps] = [
+        DailyRecaps(id: 1, recaps: [
+            TodayMeals(id: 1, name: "Dimsum", time: "20:00", timesOfEat: 1, image: "Dimsum", category: "meal", reflections: ["The taste", "It was time"], feeling: "happy", volume: 0),
+        ], date: "Friday, 8 Apr", waterVolume: 0, reflectionSummary: "It was time", feelingSummary: "happy", totalMeal: 1),
+        
+    ]
+    
     static var previews: some View {
-        DailyRecap()
+        DailyRecap(allRecap: $dummy)
     }
 }
